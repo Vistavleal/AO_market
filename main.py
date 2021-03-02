dd6d213160f06742a73a12926ac96ef7491f2c10
Vistavleal <vistavlealbox@gmail.com>
2021-03-02 21:31:32 +0300

    dev branch

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+import json
+import time
+import requests
+
+
+#url = 'https://www.albion-online-data.com/api/v2/stats/prices/T4_BAG?locations=Thetford,Caerleon&qualities=1'
+#r = requests.get(url, allow_redirects=False)
+#
+#open ('AO_db.json', "wb").write(r.content)
+
+with open('AO_db.json', 'r') as file:
+    data = json.load(file)
+
+with open('AO_db.json', 'w') as file:
+    json.dump(data, file, indent=3)
+
+
